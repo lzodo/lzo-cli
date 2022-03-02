a79f5b93fe6c102fe6ca821f9c786c93d896d7e6
liaozhongxun <869664233@qq.com>
2022-03-02 17:22:27 +0800

    up

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const vueCommands = require("./lib/core/commands/vue-cmd");
 const configCommands = require("./lib/core/commands/config-cmd");
 const systemCommands = require("./lib/core/commands/system-cmd");
 
+// import chalk from "chalk";
+// console.log(chalk.blue('Hello world!'));
+
 // 定义 输入zo --version 或 -V 显示1.0.0, --version规则是commander里定义好的
 // program.version("1.0.0")
 program.version(require("./package.json").version)
@@ -25,3 +28,7 @@ systemCommands()
 
 // 继续传入的参数
 program.parse(process.argv)
+
+
+
+//chalk 
