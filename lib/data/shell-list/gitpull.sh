cbde6a95030b86ad0bcb894e229f96ae2513f649
liaozhongxun <869664233@qq.com>
2022-08-26 16:54:36 +0800

    2022-08-26 16:54提交上传

--- lib/data/shell-list/gitpull.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# DIRLIST=("/mnt/d/lzo-project/lzo-shell-scripts" "/mnt/d/lzo-project/lzo-docs-blog-2" "/mnt/d/lzo-project/lzo-web-project")
-DIRLIST=(1 2 3)
-echo $DIRLIST
-for(( i=0; i<=${#DIRLIST[@]}; i++ ))
-do
-    echo $DIRLIST[$i]
-done 
